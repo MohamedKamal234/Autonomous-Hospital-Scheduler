4f2ff6230668c527259115c4369c2983f1ad4cb1
mohamed <fsuc4374@gamil.com>
2026-05-11 01:21:16 +0300

    n

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ st.set_page_config(
 # --- 2. إعدادات الربط مع السحابة (Supabase Connection) ---
 SUPABASE_URL = "https://kxoasyhtxsznelisxrud.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4b2FzeWJ0eHN6bnJsaXN4cnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTI5NjksImV4cCI6MjA5MzkyODk2OX0._f1FFz9vdoGecazw1Ta6wVPxAlskhZkB7K9IX0FPb0k"
-
 try:
     if 'supabase' not in st.session_state:
         st.session_state.supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
